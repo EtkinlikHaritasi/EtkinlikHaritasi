7501725b3168e1340dc67415aaa13c305351d733
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2025-05-18 16:49:10 +0300

    Yakındaki etkinlikler çekmecesine başlandı.

--- app/src/main/java/com/github/EtkinlikHaritasi/EtkinlikHaritasi/ui/pages/Keşfet.kt
+++ app/src/main/java/com/github/EtkinlikHaritasi/EtkinlikHaritasi/ui/pages/Keşfet.kt
@@ -3,8 +3,10 @@ package com.github.EtkinlikHaritasi.EtkinlikHaritasi.ui.pages
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.fonts.FontStyle
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -13,10 +15,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.FontScaling
 import androidx.core.app.ActivityCompat
 import com.github.EtkinlikHaritasi.EtkinlikHaritasi.LocationUtils
+import com.github.EtkinlikHaritasi.EtkinlikHaritasi.ui.theme.Typography
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.maps.android.compose.*
+import kotlinx.serialization.descriptors.StructureKind
 
 class Keşfet
 {
@@ -24,6 +30,7 @@ class Keşfet
     val kapalıyken_simge = Icons.Outlined.Map
     val başlık = "Keşfet"
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun İçerik(modifier: Modifier = Modifier)
     {
@@ -32,15 +39,64 @@ class Keşfet
             LocationUtils.getFusedLocationProviderClient(context)
         }
 
+        var etkinlikler:List<Unit>
+
         Box(
             modifier = modifier
         ) {
-            GoogleMap(
-                modifier = Modifier.fillMaxSize(),
-                properties = MapProperties(isMyLocationEnabled = true),
-                onMyLocationButtonClick = { konumum_düğmesi(context, fusedLocationProviderClient) }
+            BottomSheetScaffold(
+                modifier = Modifier.fillMaxWidth(),
+                sheetContent = {
+                    Row()
+                    {
+                        LazyColumn(
+                            modifier = Modifier.fillMaxWidth().padding(horizontal = Dp(10f), vertical = Dp(5f)),
+                            verticalArrangement = Arrangement.spacedBy(space = Dp(20f))
+                        )
+                        {
+                            item {
+                                Row()
+                                {
+                                    Text(
+                                        text = "Yakındaki Etkinlikler",
+                                        style = Typography.headlineMedium,
+                                    )
+                                }
+                            }
+                            item {
+                                Row() {
+                                    Icon(Icons.Filled.AcUnit, contentDescription = "kar tanesi")
+                                    Text(text = "Lorem Ipsum", style = Typography.titleMedium)
+                                }
+                            }
+                            item {
+                                Row() {
+                                    Icon(Icons.Filled.Gavel, contentDescription = "tokmak")
+                                    Text(text = "Dolor Sit Amet", style = Typography.titleMedium)
+                                }
+                            }
+                        }
+                    }
+                }
             )
+            {
+
+                GoogleMap(
+                    modifier = Modifier.fillMaxSize(),
+                    properties = MapProperties(isMyLocationEnabled = true),
+                    contentPadding = PaddingValues(bottom = Dp(50f)),
+                    onMyLocationButtonClick = {
+                        konumum_düğmesi(
+                            context,
+                            fusedLocationProviderClient
+                        )
+                    }
+                )
+
+            }
+
         }
+
     }
 
     fun konumum_düğmesi(context: Context,
