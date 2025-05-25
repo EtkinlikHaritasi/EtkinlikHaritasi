7a3db95ceb9c896a27e3cdb2a0da65e871418bd9
rukiyeberna <rb.turan@hotmail.com>
2025-05-25 17:41:35 +0300

    implement MVVM structure for Event caching with Room and LiveData

--- app/src/main/java/com/github/EtkinlikHaritasi/EtkinlikHaritasi/localdb/dao/EventDao.kt
+++ app/src/main/java/com/github/EtkinlikHaritasi/EtkinlikHaritasi/localdb/dao/EventDao.kt
@@ -1,13 +1,27 @@
 package com.github.EtkinlikHaritasi.EtkinlikHaritasi.localdb.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.github.EtkinlikHaritasi.EtkinlikHaritasi.localdb.entity.Event
 
 @Dao
 interface EventDao {
-    @Insert
-    suspend fun insert(event: Event)
+//    @Insert
+//    suspend fun insert(event: Event)
 
-    // TODO: implement full CRUD
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(events: List<Event>)
+
+    @Query("SELECT * FROM event_table ORDER BY eventId ASC")
+    fun getAllEvents(): LiveData<List<Event>>
+
+    @Query("DELETE FROM event_table")
+    suspend fun clearAll()
+
+
+
+
 }
--- app/src/main/java/com/github/EtkinlikHaritasi/EtkinlikHaritasi/repository/EventRepository.kt
+++ app/src/main/java/com/github/EtkinlikHaritasi/EtkinlikHaritasi/repository/EventRepository.kt
@@ -1,11 +1,26 @@
 package com.github.EtkinlikHaritasi.EtkinlikHaritasi.repository
 
+import androidx.lifecycle.LiveData
+import com.github.EtkinlikHaritasi.EtkinlikHaritasi.localdb.dao.EventDao
 import com.github.EtkinlikHaritasi.EtkinlikHaritasi.localdb.entity.Event
 import com.github.EtkinlikHaritasi.EtkinlikHaritasi.remote.RetrofitInstance
 import com.github.EtkinlikHaritasi.EtkinlikHaritasi.remote.api.EventApi
 
-class EventRepository {
+class EventRepository(private val eventDao: EventDao){
     private val api = RetrofitInstance.retrofit.create(EventApi::class.java)
+
+    val allEvents: LiveData<List<Event>> = eventDao.getAllEvents()
+
+    //room icin
+    suspend fun refreshEventsFromApi() {
+        val response = api.getAllEvents()
+        if (response.isSuccessful) {
+            response.body()?.let { events ->
+                eventDao.clearAll()
+                eventDao.insertAll(events)
+            }
+        }
+    }
 
     suspend fun getEvents() = api.getAllEvents()
 
