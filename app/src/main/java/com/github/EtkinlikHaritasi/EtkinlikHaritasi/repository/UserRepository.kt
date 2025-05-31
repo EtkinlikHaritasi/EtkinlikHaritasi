187967c386de42daced34128f64fa4c7cedb7860
mehmetfadil <mfadilboyaci@gmail.com>
2025-05-31 20:41:49 +0300

    event entity'sine location double dizi olarak eklendi, user entity'sine password ve etkinlikler için listeler konuldu, UserRepository loginUser fonksiyonu eklendi, event ve userlar getirilirken nesne liste dönüşümü yapıldı

--- app/src/main/java/com/github/EtkinlikHaritasi/EtkinlikHaritasi/repository/UserRepository.kt
+++ app/src/main/java/com/github/EtkinlikHaritasi/EtkinlikHaritasi/repository/UserRepository.kt
@@ -3,11 +3,23 @@ package com.github.EtkinlikHaritasi.EtkinlikHaritasi.repository
 import com.github.EtkinlikHaritasi.EtkinlikHaritasi.localdb.entity.User
 import com.github.EtkinlikHaritasi.EtkinlikHaritasi.remote.RetrofitInstance
 import com.github.EtkinlikHaritasi.EtkinlikHaritasi.remote.api.UserApi
+import retrofit2.Response
 
 class UserRepository {
     private val api = RetrofitInstance.retrofit.create(UserApi::class.java)
 
-    suspend fun getUsers() = api.getAllUsers()
+    suspend fun getUsersMap(): Response<Map<String, User>> {
+        return api.getAllUsers()
+    }
+
+    suspend fun getAllUsersList(): List<User>? {
+        val mapResp = api.getAllUsers()
+        return if (mapResp.isSuccessful) {
+            mapResp.body()?.values?.toList()
+        } else {
+            null
+        }
+    }
 
     suspend fun getUser(id: Int) = api.getUserById(id)
 
@@ -16,4 +28,10 @@ class UserRepository {
     suspend fun updateUser(id: Int, user: User) = api.updateUser(id, user)
 
     suspend fun deleteUser(id: Int) = api.deleteUser(id)
+
+    // loginUser’u da “liste formatını” kullanan şekle çevirelim
+    suspend fun loginUser(email: String, password: String): User? {
+        val usersList = getAllUsersList() // direkt null veya List<User>
+        return usersList?.find { it.email == email && it.password == password }
+    }
 }
